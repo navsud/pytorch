530c80b3881b95fddc50c64722ce240c76dc3139
PaulZhang12 <paulzhan@fb.com>
2025-09-09 07:56:53 -0700

    Increase persistent reduction x block
    ghstack-source-id: c8eaa61045b9f7704c69d34a6c8b22df07a018b1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/162446

--- torch/_inductor/runtime/triton_heuristics.py
+++ torch/_inductor/runtime/triton_heuristics.py
@@ -1935,7 +1935,6 @@ class DebugAutotuner(CachingAutotuner):
             kernel_name = f"{max(possible_names, key=len)}"
             if not re.match(self.regex_filter, kernel_name):
                 return
-
             if len(self.launchers) != 1:
                 if len(self.launchers) == 0:
                     start_time = time.time_ns()
@@ -2892,8 +2891,26 @@ def _persistent_reduction_configs(
     if "y" in size_hints:
         pass
     # TODO(jansel): we should be able to improve these heuristics
-    elif reduction_hint == ReductionHint.INNER and rnumel >= 256:
-        configs = configs[:1]
+    elif reduction_hint == ReductionHint.INNER:
+        if rnumel > 1024:
+            configs = configs[:1]
+        elif rnumel <= 512:
+            configs = [
+                triton_config_reduction(
+                    size_hints,
+                    8,
+                    rnumel,
+                )
+            ]
+        else:
+            configs = [
+                triton_config_reduction(
+                    size_hints,
+                    4,
+                    rnumel,
+                )
+            ]
+
     elif reduction_hint == ReductionHint.OUTER:
         configs = configs[-1:]
     elif reduction_hint == ReductionHint.OUTER_TINY:
